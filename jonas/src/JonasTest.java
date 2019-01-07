d61dda4f5fadaa28c38bd6beb200dd30d566d9a9
simon-lammes <simon.lammes@cofinpro.de>
2019-01-07 15:06:53 +0100

    blablabla

--- jonas/src/JonasTest.java
+++ jonas/src/JonasTest.java
@@ -1,5 +1,5 @@
 public class JonasTest {
-    public static void main(String[]args){
-        System.out.println("Test");
+    public static void main(String[] args){
+        System.out.println("blablabla");
     }
 }
