8bb57dc731a0cb0bc868ebaaacc3870c0edd4822
Philipp Mayer <philipp.mayer@cofinpro.de>
2019-01-09 11:26:43 +0100

    added project BowlingGame

---
+++ BowlingGame/src/BowlingGame.java
@@ -0,0 +1,51 @@
+/**
+ * Represents a bowling game
+ * @author Team DevMatch - Cofinpro AG
+ */
+public class BowlingGame {
+
+    /**
+     * Constucts a BowlingGame with 10 pins
+     */
+    public BowlingGame(){
+
+    }
+
+    /**
+     *Constucts a BowlingGame with a custom amount of pins
+     * @param count defines the amount of pins
+     */
+    public BowlingGame(int count){
+
+    }
+
+    /**
+     *resets the game by putting all pins back up
+     */
+    public void reset(){
+
+    }
+
+    /**
+     * knocks out a random number of pins
+     * @return number of knocked out pins
+     */
+    public int rollBall(){
+        return 0;
+    }
+
+    /**
+     * counts standing pins
+     * @return amount of standing pins
+     */
+    public int countStandingPins(){
+        return 0;
+    }
+
+    /**
+     * prints out a visualization of the BowlingGame
+     */
+    public void visualizeStandingPins(){
+
+    }
+}
---
+++ BowlingGame/src/Pin.java
@@ -0,0 +1,26 @@
+/**
+ * represents a pin
+ * @author Team DevMatch - Cofinpro AG
+ */
+public class Pin {
+    /**
+     * creates a standing pin
+     */
+    public Pin(){
+
+    }
+
+    /**
+     * changes the state of the pin from STANDING to KNOCKEDOUT
+     */
+    public void knockOut(){
+
+    }
+
+    /**
+     * changes the state of the pin from KNOCKEDOUT to STANDING
+     */
+    public void putPinBackUp(){
+
+    }
+}
