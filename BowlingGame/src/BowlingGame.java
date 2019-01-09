48d5051fa80865eb1b63483a2dc9c9897b50d7d9
Philipp Mayer <philipp.mayer@cofinpro.de>
2019-01-09 17:47:21 +0100

    finished project BowlingGame (tests missing)

--- BowlingGame/src/BowlingGame.java
+++ BowlingGame/src/BowlingGame.java
@@ -4,11 +4,12 @@
  */
 public class BowlingGame {
 
+    private Pin[] pinArray;
     /**
      * Constucts a BowlingGame with 10 pins
      */
     public BowlingGame(){
-
+        this(10);
     }
 
     /**
@@ -16,36 +17,94 @@ public class BowlingGame {
      * @param count defines the amount of pins
      */
     public BowlingGame(int count){
-
+        pinArray = new Pin[count];
+        for(int amount=0; amount<count; amount++) {
+            pinArray[amount] = new Pin();
+        }
     }
 
     /**
      *resets the game by putting all pins back up
      */
     public void reset(){
-
+        for(int count=0; count<pinArray.length; count++){
+            pinArray[count].putPinBackUp();
+        }
     }
 
     /**
      * knocks out a random number of pins
      * @return number of knocked out pins
      */
-    public int rollBall(){
-        return 0;
+    public String rollBall(){
+        int knockedOutPinsByCurrentBall = 0;
+        for(int count=0; count<pinArray.length; count++) {
+            int knockDownPin = (int)(Math.random()*2);
+            if (knockDownPin == 1 && pinArray[count].pinState == PinState.STANDING) {
+                pinArray[count].knockOut();
+                knockedOutPinsByCurrentBall++;
+            }
+        }
+        return "" + knockedOutPinsByCurrentBall + " pin/s were knocked out by the current ball";
     }
 
     /**
      * counts standing pins
      * @return amount of standing pins
      */
-    public int countStandingPins(){
-        return 0;
+
+    public String countStandingPins(){
+        int standingPins = 0;
+        for(int count=0; count<pinArray.length; count++){
+            if(pinArray[count].getPinState() == PinState.STANDING)
+                standingPins++;
+        }
+        return "" + standingPins + " pin/s are still standing";
     }
 
     /**
      * prints out a visualization of the BowlingGame
      */
-    public void visualizeStandingPins(){
+    public void visualizeStandingPins(int amountPins){
+        int tableRows = (int) (-1/2 + Math.sqrt(1/4 + 2*amountPins));//pq to calculate the tableRows (visualization)
+        if((-1/2 + Math.sqrt(1/4 + 2*amountPins)%1.0 != 0)) // round up if result is double (eg. result: 1.2 would be rounded to 1
+            tableRows++;                                    // now a special row is added for the pins that dont fill the entire last row
+        int pinCount = 0;
 
+        System.out.println("View on pins from top");
+        for(int countRow=0; countRow<tableRows; countRow++){
+            for(int countSpaces=0; countSpaces<(tableRows - countRow); countSpaces++)
+                System.out.print(" ");
+            for(int countColumn=1; countColumn<=countRow+1; countColumn++){
+                if(pinCount>=amountPins)
+                    break;
+                if(pinArray[pinCount].getPinState() == PinState.STANDING)
+                    System.out.print("o ");
+                if(pinArray[pinCount].getPinState() == PinState.KNOCKEDOUT)
+                    System.out.print("8 ");
+                pinCount++;
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int amountBowlingBalls = 3;
+        int amountPins =11;
+        if(amountPins<1 || amountPins>99)
+            throw new IllegalArgumentException("Amount of pins must be between 1 and 99");
+
+        BowlingGame bowlingGame = new BowlingGame(amountPins);
+
+        bowlingGame.reset();
+
+        for(int count = 0; count<amountBowlingBalls; count++){
+            System.out.println("_______________________________________________");
+            System.out.println("Round: " + (count+1));
+            System.out.println(bowlingGame.rollBall());
+            System.out.println(bowlingGame.countStandingPins());
+            bowlingGame.visualizeStandingPins(amountPins);
+        }
     }
 }
--- BowlingGame/src/Pin.java
+++ BowlingGame/src/Pin.java
@@ -3,6 +3,7 @@
  * @author Team DevMatch - Cofinpro AG
  */
 public class Pin {
+    PinState pinState = PinState.STANDING;
     /**
      * creates a standing pin
      */
@@ -14,13 +15,22 @@ public class Pin {
      * changes the state of the pin from STANDING to KNOCKEDOUT
      */
     public void knockOut(){
-
+        pinState = PinState.KNOCKEDOUT;
     }
 
     /**
      * changes the state of the pin from KNOCKEDOUT to STANDING
      */
     public void putPinBackUp(){
+        pinState = PinState.STANDING;
+    }
+
+    /**
+     * returns wheather pin is STANDING or KNOCKEDOUT
+     * @return current state of pin
+     */
+    public PinState getPinState(){
+            return this.pinState;
 
     }
 }
