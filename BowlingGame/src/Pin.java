a55d924306007a810b65448a88949db82b7ae09f
simon-lammes <simon.lammes@cofinpro.de>
2019-01-10 15:55:02 +0100

    Created a revolutionary bowling game

--- BowlingGame/src/Pin.java
+++ BowlingGame/src/Pin.java
@@ -3,24 +3,29 @@
  * @author Team DevMatch - Cofinpro AG
  */
 public class Pin {
+    private PinState pinState;
     /**
      * creates a standing pin
      */
     public Pin(){
-
+        pinState = PinState.STANDING;
     }
 
     /**
      * changes the state of the pin from STANDING to KNOCKEDOUT
      */
     public void knockOut(){
-
+        pinState = PinState.KNOCKEDOUT;
     }
 
     /**
      * changes the state of the pin from KNOCKEDOUT to STANDING
      */
     public void putPinBackUp(){
+        pinState = PinState.STANDING;
+    }
 
+    public PinState getPinState() {
+        return pinState;
     }
 }
