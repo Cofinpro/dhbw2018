59d133e78985875e25d5baec769e2398970b56f2
Philipp Mayer <philipp.mayer@cofinpro.de>
2019-01-14 17:48:57 +0100

    addierer Test und Palindrom + Test Klassen hinzugefügt

--- RekursiveFunktionen/src/Addierer.java
+++ RekursiveFunktionen/src/Addierer.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class Addierer {
+    public Addierer(){
+        double a;
+        double b;
+
+    }
+    public double addierePrimitiv(double a, double b){
+        return a+b;
+    }
+
+    public double addiereGekapselt(double a, double b){
+        Double aDouble = new Double(a);
+        Double bDouble = new Double(b);
+        return aDouble + bDouble;
+    }
+
+    public double addiereRekursiv(double a, double b){
+        if(b == 0)
+            return a;
+        if(b>0)
+            return addiereRekursiv(a,b-1) + 1;
+        throw new IllegalArgumentException("Keine Subtraktion möglich");
+    }
 }
+
+
+/*
+
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Geben sie die erste Zahl ein: ");
+                double a = scanner.nextDouble();
+                System.out.println("Geben sie die zweite Zahl ein: ");
+                double b = scanner.nextDouble(); */
