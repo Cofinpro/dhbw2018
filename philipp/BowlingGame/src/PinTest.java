819b85b68bdabb0e6f965a2ea4c4c2c8ca861b02
Philipp Mayer <philipp.mayer@cofinpro.de>
2019-01-15 14:03:18 +0100

    fixed my entire directory system

--- philipp/BowlingGame/src/PinTest.java
+++ philipp/BowlingGame/src/PinTest.java
@@ -1,6 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.*;
+
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 
 
 public class PinTest {
@@ -8,14 +9,14 @@ public class PinTest {
     @Test
     public void pin() {
         Pin pin = new Pin();
-        Assert.assertThat(pin.getPinState(), is(PinState.STANDING));
+        Assert.assertEquals(pin.getPinState(), is(PinState.STANDING));
     }
 
     @Test
     public void knockOut() {
         Pin pin = new Pin();
         pin.knockOut();
-        Assert.assertThat(pin.getPinState(), is(PinState.KNOCKEDOUT));
+        Assert.assertEquals(pin.getPinState(), is(PinState.KNOCKEDOUT));
     }
 
     @Test
@@ -23,6 +24,6 @@ public class PinTest {
         Pin pin = new Pin();
         pin.knockOut();
         pin.putPinBackUp();
-        Assert.assertThat(pin.getPinState(), is(PinState.STANDING));
+        Assert.assertEquals(pin.getPinState(), is(PinState.STANDING));
     }
 }
