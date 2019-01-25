70f29d1e81fe18381fadd7b0cb3e62c675d9d7fd
jonastrautmann <Jonas.Trautmann@cofinpro.de>
2019-01-25 11:16:50 +0100

    Merge branch 'master' of https://github.com/Cofinpro/dhbw2018
    # Conflicts:
    #	Aufgaben/Aufgaben.iml
    #	Aufgaben/Koordinatensystem/Koordinatensystem.iml
    #	Aufgaben/tikTak/src/HelloWorld.java
    #	philipp/philipp.iml
    #	simon/PeriodicTable/PeriodicTable.iml

--- Aufgaben/src/TikTakKleinerAlsNullException.java
+++
@@ -1,8 +0,0 @@
-class TikTakKleinerNullException extends IllegalArgumentException {
-    TikTakKleinerNullException() {
-        super("Die Zahl ist kleiner als Null");
-    }
-    TikTakKleinerNullException(String fehlermeldung) {
-        super(fehlermeldung);
-    }
-}
