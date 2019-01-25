01930a93a7ce43ac01cdfe090d1729cd5493225a
simon-lammes <simon.lammes@cofinpro.de>
2019-01-25 17:40:59 +0100

    changed the name customView to bankAccountSuperficialView because this is more meaningful; changed its structure to be suitable for being used as a custom control

---
+++ teamDevMatch/LegoCityBank/src/controller/BankAccountSuperficialViewControl.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class BankAccountSuperficialViewControl {
+
+    @FXML private TextField bankBalanceTextField;
+    @FXML private TextField accountNumberTextField;
+    @FXML private TextField accountTypeTextField;
+
+}
