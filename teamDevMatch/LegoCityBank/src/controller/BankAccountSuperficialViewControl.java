2c071579324dfbe93f66a092c54d042557fe6981
simon-lammes <simon.lammes@cofinpro.de>
2019-01-26 19:07:56 +0100

    now bank accounts are displayed in the dashboard view

--- teamDevMatch/LegoCityBank/src/controller/BankAccountSuperficialViewControl.java
+++ teamDevMatch/LegoCityBank/src/controller/BankAccountSuperficialViewControl.java
@@ -1,14 +1,41 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import models.BankAccount;
+import views.CustomMain;
 
-public class BankAccountSuperficialViewControl {
+import java.io.IOException;
+
+public class BankAccountSuperficialViewControl extends AnchorPane {
 
     @FXML private TextField bankBalanceTextField;
     @FXML private TextField accountNumberTextField;
     @FXML private TextField accountTypeTextField;
 
-    @FXML
+    private BankAccount bankAccount;
+
+    public BankAccountSuperficialViewControl(BankAccount bankAccount) {
+        FXMLLoader loader = new FXMLLoader(new CustomMain().getClass().getResource("bankAccountSuperficialView.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.bankAccount = bankAccount;
+        updateView();
+    }
+
+    private void updateView() {
+        bankBalanceTextField.setText(String.valueOf(bankAccount.getBalance()));
+        accountNumberTextField.setText(bankAccount.getBankAccountNumber());
+        accountTypeTextField.setText("Volksbank Girokonto");
+    }
+
 
 }
--- teamDevMatch/LegoCityBank/src/controller/DashboardController.java
+++ teamDevMatch/LegoCityBank/src/controller/DashboardController.java
@@ -3,6 +3,9 @@ package controller;
 import daos.UserDao;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import models.BankAccount;
+import models.Customer;
 import models.GiroAccount;
 import models.User;
 
@@ -11,6 +14,7 @@ public class DashboardController {
     @FXML private TextField usernameTextField;
     @FXML private TextField fullnameTextField;
     @FXML private TextField totalBalanceTextField;
+    @FXML private VBox bankAccountsVBox;
 
     private UserDao userDao;
     private User loggedInUser;
@@ -22,6 +26,18 @@ public class DashboardController {
         usernameTextField.setText(loggedInUser.getUserName());
         fullnameTextField.setText(loggedInUser.getFirstName() + " " + loggedInUser.getLastName());
         totalBalanceTextField.setText("0.00");
+        updateBankAccountViews();
+    }
+
+    private void updateBankAccountViews() {
+        Customer loggedInCustomer = (Customer) loggedInUser;
+        if (loggedInCustomer == null) {
+            return;
+        }
+        for (BankAccount bankAccount : loggedInCustomer.getBankAccounts()) {
+            BankAccountSuperficialViewControl control = new BankAccountSuperficialViewControl(bankAccount);
+            bankAccountsVBox.getChildren().addAll(control);
+        }
     }
 
 }
