8892602932dbea49b1ccb90cd72c102ec2c3576a
simon-lammes <simon.lammes@cofinpro.de>
2019-01-28 14:35:51 +0100

    the application now has a beautiful lego logo

--- teamDevMatch/LegoCityBank/src/views/Main.java
+++ teamDevMatch/LegoCityBank/src/views/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("loginWindow.fxml"));
         primaryStage.setTitle("Lego City Bank");
+        primaryStage.getIcons().addAll(new Image("images\\lego-logo.jpg"));
         primaryStage.setScene(new Scene(root, 900,600));
         primaryStage.show();
         PRIMARY_STAGE = primaryStage;
