58dc7dcb5d9c001f7921d3db4b89ea44685fd648
simon-lammes <simon.lammes@cofinpro.de>
2019-01-28 10:31:08 +0100

    the dashboard buttons now all work

--- teamDevMatch/LegoCityBank/src/controller/BankAccountController.java
+++ teamDevMatch/LegoCityBank/src/controller/BankAccountController.java
@@ -2,12 +2,15 @@ package controller;
 
 import daos.UserDao;
 import helper.OutputHelper;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import models.BankAccount;
 import models.Customer;
 
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 public class BankAccountController {
@@ -40,4 +43,15 @@ public class BankAccountController {
         userDao.deleteBankAccount(customer, bankAccount);
     }
 
+    public void deposit(ActionEvent actionEvent) throws IOException {
+        OutputHelper.setNextScene("depositWindow.fxml");
+    }
+
+    public void disburse(ActionEvent actionEvent) throws  IOException {
+        OutputHelper.setNextScene("disburseWindow.fxml");
+    }
+
+    public void goBack(MouseEvent mouseEvent) throws IOException {
+        OutputHelper.setNextScene("dashboardWindow.fxml");
+    }
 }
