3a4397a05de37bf6367779629dcab52cebafe760
simon-lammes <simon.lammes@cofinpro.de>
2019-01-29 16:34:40 +0100

    disburse now works

--- teamDevMatch/LegoCityBank/src/controller/DisburseController.java
+++ teamDevMatch/LegoCityBank/src/controller/DisburseController.java
@@ -14,20 +14,35 @@ import java.io.IOException;
 public class DisburseController {
 
     @FXML private TextField disburseValueTextField;
+    @FXML private TextField errorTextField;
 
     public void goBack(Event event) throws IOException {
         OutputHelper.setNextScene("bankAccountView.fxml");
+        disburseValueTextField.textProperty().addListener(e -> validateInput());
+    }
+
+    private void validateInput() {
+        String input = disburseValueTextField.getText();
+        String validInput = input.replaceAll("[^0-9]", "");
+        if (!validInput.equals(input)) {
+            disburseValueTextField.setText(validInput);
+        }
     }
 
     public void disburse(Event event) throws IOException {
         String input = disburseValueTextField.getText();
-        try {
-            double value = Double.parseDouble(input);
+        double value = Double.parseDouble(input);
+        if (value <= 0) {
+            errorTextField.setText("Du kannst nur positive Beträge abheben.");
+        } else {
             BankAccount bankAccount = CustomerManager.getInstance().getInspectedBankAccount();
-            bankAccount.disburse(value);
-            goBack(event);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
+            if (value > bankAccount.getBalance()) {
+                errorTextField.setText("Du kannst maximal " + bankAccount.getBalance() + " abheben.");
+            } else {
+                bankAccount.disburse(value);
+                goBack(event);
+            }
         }
+
     }
 }
