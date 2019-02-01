007994f6b4e8a2c7410f9421b7516e6c6a844765
Phape <phape@icloud.com>
2019-02-01 13:28:16 +0100

    array is now created by the new constructor of the class

--- teamDevMatch/Benchmark/src/helper/IntegerArrayHelper.java
+++ teamDevMatch/Benchmark/src/helper/IntegerArrayHelper.java
@@ -7,7 +7,7 @@ public class IntegerArrayHelper {
 
     private Integer[] intArray;
 
-    public void createIntArray(int maxLength) {
+    public IntegerArrayHelper (int maxLength) {
         intArray = new Integer[maxLength];
         Random random = new Random();
         for (int i = 0; i < maxLength; i++) {
@@ -17,7 +17,7 @@ public class IntegerArrayHelper {
 
     public Integer[] getIntArray(int length) {
         if (length > intArray.length)
-            throw new IllegalArgumentException("The array created with createIntArray method is too short");
+            throw new IllegalArgumentException("The array created by the constructor is too short");
         Integer[] part = Arrays.copyOfRange(intArray, 0,length);
 
         return part;
