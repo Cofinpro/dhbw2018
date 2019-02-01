2e72281e0e3ac7f77ae259758edfee1115ae1be6
Phape <phape@icloud.com>
2019-02-01 13:20:51 +0100

    wrote methods to create an array and to get parts of that array

--- teamDevMatch/Benchmark/src/helper/IntegerArrayHelper.java
+++ teamDevMatch/Benchmark/src/helper/IntegerArrayHelper.java
@@ -1,4 +1,25 @@
 package helper;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class IntegerArrayHelper {
+
+    private Integer[] intArray;
+
+    public void createIntArray(int maxLength) {
+        intArray = new Integer[maxLength];
+        Random random = new Random();
+        for (int i = 0; i < maxLength; i++) {
+            intArray[i] = random.nextInt(Integer.MAX_VALUE);
+        }
+    }
+
+    public Integer[] getIntArray(int length) {
+        if (length > intArray.length)
+            throw new IllegalArgumentException("The array created with createIntArray method is too short");
+        Integer[] part = Arrays.copyOfRange(intArray, 0,length);
+
+        return part;
+    }
 }
