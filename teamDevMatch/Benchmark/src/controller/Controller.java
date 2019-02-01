1fa560938f649d4517e92e94e415c97103c41095
simon-lammes <simon.lammes@cofinpro.de>
2019-02-01 12:20:11 +0100

    cleaner code

--- teamDevMatch/Benchmark/src/controller/Controller.java
+++ teamDevMatch/Benchmark/src/controller/Controller.java
@@ -5,24 +5,24 @@ import interfaces.Sorter;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import sorting.JonasSort;
+import sorting.PhilippSort;
 import sorting.SimonSort;
 
 public class Controller {
 
     private static final int startArrayLength = 1;
-    private static final int endArrayLength = 10000;
-    private static final int increment = 1000;
+    private static final int endArrayLength = 1000;
+    private static final int increment = 100;
 
     @FXML
     private LineChart<Integer, Integer> lineChart;
 
     @FXML
     public void initialize() {
-        lineChart.getData().add(createSimonsResults());
-    }
-
-    private XYChart.Series<Integer, Integer> createSimonsResults() {
-        return createResults(new SimonSort<>());
+        lineChart.getData().add(createResults(new SimonSort<>()));
+        //lineChart.getData().add(createResults(new JonasSort<>()));
+        lineChart.getData().add(createResults(new PhilippSort<>()));
     }
 
     private XYChart.Series<Integer, Integer> createResults(Sorter<Integer> sorter) {
