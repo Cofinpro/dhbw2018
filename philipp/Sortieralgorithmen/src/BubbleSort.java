edd4220fc98a565988be4c51807ff712e80af632
Phape <phape@icloud.com>
2019-02-01 10:56:38 +0100

    added sorting algorithms

--- philipp/Sortieralgorithmen/src/BubbleSort.java
+++ philipp/Sortieralgorithmen/src/BubbleSort.java
@@ -1,27 +1,24 @@
-import java.util.Arrays;
-
-public class BubbleSort {
+public class BubbleSort<T extends Comparable<T>> {
 
     public static void main(String[] args) {
-        int[] zahlenFeld = {271, -21, 10, 7, 2, 77, 42, 12};
-        Arrays.sort(zahlenFeld);
-        sort(zahlenFeld);
-        for (int i=0; i<zahlenFeld.length; i++) {
-            System.out.println(zahlenFeld[i]);
+        Integer[] intArray = {271, -21, 10, 7, 2, 77, 42, 12};
+        //Arrays.sort(intArray);
+        BubbleSort<Integer> bubbleSort = new BubbleSort<>();
+        bubbleSort.bubbleSort(intArray);
+        for (Integer integer : intArray) {
+            System.out.println(integer);
         }
     }
 
-    public static int[] sort(int[] zahlenFeld) {
-        int temp;
-        for (int i = 0; i < zahlenFeld.length; i++) {
-            for (int j = 1; j < zahlenFeld.length-i; j++) {
-                if (zahlenFeld[j] < zahlenFeld[j-1]) {
-                    temp = zahlenFeld[j-1];
-                    zahlenFeld[j-1] = zahlenFeld[j];
-                    zahlenFeld[j] = temp;
+    public void bubbleSort(T[] input) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 1; j < input.length-i; j++) {
+                if (input[j].compareTo(input[j-1]) < 0) {
+                    T temp = input[j-1];
+                    input[j-1] = input[j];
+                    input[j] = temp;
                 }
             }
         }
-        return zahlenFeld;
     }
 }
---
+++ philipp/Sortieralgorithmen/src/PhilippSort.java
@@ -0,0 +1,24 @@
+public class PhilippSort<T extends Comparable<T>> {
+
+    public static void main(String[] args) {
+
+        Integer[] intArray = {271, -21, 10, 7, 2, 77, 42, 12};
+        PhilippSort<Integer> philippSort = new PhilippSort<>();
+        philippSort.selectionSort(intArray);
+        for (Integer integer : intArray) {
+            System.out.println(integer);
+        }
+    }
+
+    public void selectionSort(T[] input) {
+        for (int i = 0; i < input.length - 1; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[i].compareTo(input[j]) > 0) {
+                    T temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
+                }
+            }
+        }
+    }
+}
