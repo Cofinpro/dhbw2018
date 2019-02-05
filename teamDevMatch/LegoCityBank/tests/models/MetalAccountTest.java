6b78a0cbb69bc0df6223492fdf4b9b53c6a0bb8f
jonastrautmann <Jonas.Trautmann@cofinpro.de>
2019-02-05 15:24:44 +0100

    added MetalAccounTest added MetalAccountTest

--- teamDevMatch/LegoCityBank/tests/models/MetalAccountTest.java
+++ teamDevMatch/LegoCityBank/tests/models/MetalAccountTest.java
@@ -10,6 +10,8 @@ class MetalAccountTest {
 
     MetalAccount metalAccount = new MetalAccount(customer);
 
+    GoldPrice goldPrice = GoldPrice.getInstance();
+
     @Test
     void getAccountType() {
         assertEquals(metalAccount.getAccountType(),"MetalAccount");
@@ -28,6 +30,38 @@ class MetalAccountTest {
     @Test
     void getMonthlyFeesAbsolute() {
         assertEquals(metalAccount.getMonthlyFeesAbsolute(),100.0);
+    }
+
+    @Test
+    void getBalance(){
+        double goldAmountInGramHelper = metalAccount.getGoldAmountInGram();
+        double actualBalance = goldPrice.calculateWorth(goldAmountInGramHelper);
+
+        assertEquals(metalAccount.getBalance(),actualBalance);
+    }
+
+    @Test
+    void deposit(){
+        double beforeDepositInGram = 10;
+        double depositAmountInDollar = 20;
+        double depositAmountInGram = depositAmountInDollar / goldPrice.getDollarPerGramOfGold();
+        double afterDepositInGram = beforeDepositInGram  + depositAmountInGram ;
+
+
+        metalAccount.deposit(20);
+        double help = beforeDepositInGram + metalAccount.getGoldAmountInGram();
+        assertEquals(help,afterDepositInGram);
+    }
+    @Test
+    void disburse(){
+        double beforeDepositInGram = 10;
+        double depositAmountInDollar = 20;
+        double depositAmountInGram = depositAmountInDollar / goldPrice.getDollarPerGramOfGold();
+        double afterDepositInGram = beforeDepositInGram  - depositAmountInGram ;
+
+        metalAccount.disburse(20);
+        double help = beforeDepositInGram + metalAccount.getGoldAmountInGram();
+        assertEquals(help,afterDepositInGram);
     }
 
 }
