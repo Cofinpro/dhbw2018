7e2e4dfdd2b875428c8b13bc3c5974ec13690e5b
simon-lammes <simon.lammes@cofinpro.de>
2019-02-09 17:38:42 +0100

    delete incomplete project

--- simon/Minesweeper/src/application/controller/GameCellController.java
+++
@@ -1,9 +0,0 @@
-package application.controller;
-
-import application.views.GameCellView;
-
-public class GameCellController {
-    public GameCellController(GameCellView gameCellView) {
-        gameCellView.setOnAction(e -> gameCellView.getGameCell().reveal());
-    }
-}
