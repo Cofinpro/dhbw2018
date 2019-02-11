eb7ff148447462c2dc28278444b38e6e856a5f94
jonastrautmann <Jonas.Trautmann@cofinpro.de>
2019-02-11 16:32:30 +0100

    changed Cell

--- jonas/minesweeper/src/model/Cell.java
+++ jonas/minesweeper/src/model/Cell.java
@@ -1,4 +1,26 @@
 package model;
 
 public class Cell {
+
+    private boolean getExplosive;
+    private boolean isMarked;
+    private boolean isCleared;
+    private Game game;
+    private int row;
+    private int column;
+
+    Cell(boolean getExplosive, Game game, int row, int column){
+        this.getExplosive = getExplosive;
+        this.game = game;
+        this.row = row;
+        this.column = column;
+    }
+
+    public boolean getMarkedState(){
+        return isMarked;
+    }
+    public boolean getState(){
+        return isCleared;
+    }
+
 }
