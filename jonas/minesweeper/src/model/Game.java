e6f415823315ddc11cc2bdca89784392f8f15a73
jonastrautmann <Jonas.Trautmann@cofinpro.de>
2019-02-11 17:05:20 +0100

    changed Game

--- jonas/minesweeper/src/model/Game.java
+++ jonas/minesweeper/src/model/Game.java
@@ -2,5 +2,13 @@ package model;
 
 public class Game {
 
+    private static Game instance;
+
+    public static Game getInstance(){
+        if (instance == null){
+            instance = new Game();
+        }
+        return instance;
+    }
 
 }
